f9f46fb6c2211a54251dadcad2df3feb3510aacc
Stuart Chaffe <stu@stuartchaffe.co.uk>
2024-02-22 12:36:35 +1100

    Update animations and buttons

--- wp-content/themes/gruvi/page.php
+++ wp-content/themes/gruvi/page.php
@@ -3,7 +3,7 @@
 <?php while ( have_posts() ) : the_post(); ?>
 	<?php the_content(); ?>
 
-	<section class="container">
+	<section class="container animate">
 		<h1><strong>Our technology</strong></h1>
 		<h1>Entertainment with <strong>data-driven</strong> Audience insights</h1>
 
@@ -11,18 +11,38 @@
 		<p class="lead"><strong>HOW DO WE CUT THROUGH THE NOISE AND GET PEOPLE TO WATCH OUR ADS?<br />
 		How do we cut through the noise and get people to watch our ads?</strong></p>
 
-		<p>Growing up in post-World War II era Arizona, young <strong>Sammy Fabelman</strong> aspires to become a filmmaker as he reaches adolescence, but soon discovers a shattering family secret and explores how the power of films can help him see the truth.</p>
+		<div class="animated">
+			<div class="animate">Test</div>
+			<div class="animate">Test</div>
+			<div class="animate">Test</div>
+		</div>
 
-		<a href="" class="btn">learn more</a>
+		<p>Growing up in post-World War II era Arizona, young <strong>Sammy Fabelman</strong> aspires to become a filmmaker as he reaches adolescence, but soon discovers a shattering family secret and explores how the power of films can help him see the truth.</p>
+		<div class="animate">
+			<a href="" class="btn">learn more</a>
+			<a href="" class="btn btn--border">learn more</a>
+			<a href="" class="btn btn--large">Connect with us</a>
+			<a href="" class="btn btn--border btn--large">Connect with us</a>
+		</div>
+		<div class="animate">
+			<a href="" class="btn--circle">learn more</a>
+		</div>
 
 		<h2>Case studies</h2>
+		<h2><strong>Case studies</strong></h2>
 		<p>The start of the Swedish Swimrun season and a tribute to the origin of the sport. Utö and its surrounding islands will provide the scene for great races along magical trails and protected open-water swims.</p>
 
 		<h3>The Solution <strong>Topgun: Maverick</strong></h3>
 		<p>The ÖTILLÖ Swimrun insurance is a “Swimrun-Athletes-Insurance Program” with a recognized international Insurance Company. It is possible to buy this cover for single events only or for the entire Swimrun season; the season cover protects you for all the Swimrun events which will be announced later.</p>
 
+		<div class="animated animate">
+			<div class="animate">Test</div>
+			<div class="animate">Test</div>
+			<div class="animate">Test</div>
+		</div>
+
 		<h4>Account Management</h4>
-		<p class="small">The French Riviera presented itself at its best for this year's ÖTILLÖ Swimrun Cannes. Very warm and sunny weather, the biggest field in ÖTILLÖ history with almost 800 participants from 38 nations and a challenging and varied course through and around the beautiful City of Cannes made for the perfect finale to the 2023 ÖTILLÖ Swimrun World Series season.</p>
+		<p>The French Riviera presented itself at its best for this year's ÖTILLÖ Swimrun Cannes. Very warm and sunny weather, the biggest field in ÖTILLÖ history with almost 800 participants from 38 nations and a challenging and varied course through and around the beautiful City of Cannes made for the perfect finale to the 2023 ÖTILLÖ Swimrun World Series season.</p>
 	</section>
 
 <?php endwhile; ?>
--- wp-content/themes/gruvi/src/js/scripts.js
+++ wp-content/themes/gruvi/src/js/scripts.js
@@ -4,5 +4,6 @@ import './components/no-js';
 // import './components/cookie';
 import './components/unsupported-browsers';
 import './components/menu';
+import './components/animate';
 import './components/toggle';
 import './components/accordion';
